12476aedae1547fecce36c6e908b4af79e2a0dfb
hx <huangxin@wondertek.com.cn>
2021-03-20 19:27:40 +0800

    init

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,6 +1,4 @@
-import { Component } from 'react'
-import { View, Text } from '@tarojs/components'
-import { AtButton } from 'taro-ui'
+import { View } from '@tarojs/components'
 
 import "taro-ui/dist/style/components/button.scss" // 按需引入
 import './index.scss'
